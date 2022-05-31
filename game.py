24340b2c41fdaaea0a7c4d7522875f9b6d059666
ar0ne <camtbitakou@yandex.ru>
2022-05-31 16:59:45 +0530

    base container for ui elements

--- game.py
+++ game.py
@@ -1,7 +1,10 @@
+
 import pygame
 import sys
 
-BLACK = (0, 0, 0)
+from color import Color
+from stack import Container
+
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAMES_PER_SECOND = 30
@@ -10,10 +13,11 @@ class Game:
     """Game class"""
     def __init__(self, width: int, height: int):
         pygame.init()
-        self.width = width
-        self.height = height
-        self.window = pygame.display.set_mode((self.width, self.height))
+        self.WIDTH = width
+        self.HEIGHT = height
+        self.window = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         self.clock = pygame.time.Clock()
+        self.elements = []
 
     def start(self):
         """Start game cycle"""
@@ -24,13 +28,30 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-            self.window.fill(BLACK)
 
+            # fill background
+            self.window.fill(Color.BLACK.value)
+
+            # draw game elements
+            for elem in self.elements:
+                elem.draw()
+
+            # refresh display
             pygame.display.update()
 
+            # stick to frame rate
             self.clock.tick(FRAMES_PER_SECOND)
+
+    def add_element(self, element) -> None:
+        """Add element"""
+        self.elements.append(element)
 
 
 if __name__ == "__main__":
     game = Game(WINDOW_WIDTH, WINDOW_HEIGHT)
+    container = Container(game.window, 100, 50, 100, 75, 4)
+    container.stack.push(Color.BLUE)
+    container.stack.push(Color.RED)
+    container.stack.push(Color.GREEN)
+    game.add_element(container)
     game.start()
--- stack.py
+++ stack.py
@@ -1,3 +1,62 @@
-"""
+"""Stack"""
 
-"""
+from collections import deque
+from typing import Optional
+
+import pygame
+from color import Color
+
+
+class Stack:
+    def __init__(self, max_length: int = 4) -> None:
+        """Init stack"""
+        self.max_length = max_length
+        self.elements = deque(maxlen=self.max_length)
+
+    def push(self, value: Color) -> None:
+        """Add element to stack"""
+        if len(self.elements) < self.max_length:
+            self.elements.append(value)
+
+    def pop(self) -> Optional[Color]:
+        """Pop element from stack"""
+        if self.elements:
+            return self.elements.pop()
+
+    def __iter__(self):
+        for el in list(self.elements):
+            yield el
+
+
+class Container:
+    """Container for elements"""
+
+    def __init__(
+        self,
+        window,
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        size: int,
+    ) -> None:
+        self.window = window
+        self.block_width = width
+        self.block_height = height
+        self.size = size
+        self.x = x
+        self.y = y
+        self.stack = Stack(self.size)
+        self.rect = pygame.Rect(self.x, self.y, self.block_width, self.block_height * self.size)
+
+    def draw(self) -> None:
+        for idx, color in enumerate(self.stack.elements):
+            pygame.draw.rect(
+                self.window,
+                color.value,
+                (self.x,
+                self.y + idx * self.block_height,
+                self.block_width,
+                self.block_height),
+                0
+            )
