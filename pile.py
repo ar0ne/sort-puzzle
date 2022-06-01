4899e63ec595d6206ae942e6bca7b8fa4eb828d5
ar0ne <camtbitakou@yandex.ru>
2022-06-01 13:19:01 +0530

    fixes

--- pile.py
+++ pile.py
@@ -1,33 +1,33 @@
 """Stack"""
 
 from collections import deque
-from typing import Callable, Optional
+from typing import Callable, Optional, Any
 
 import pygame
 from color import Color
 
 
 class Stack:
-    """Dummy implementation stack"""
+    """Simple stack"""
 
     def __init__(self, max_length: int = 4) -> None:
         """Init stack"""
         self.max_length = max_length
         self.elements = deque(maxlen=self.max_length)
 
-    def push(self, value: Color) -> bool:
+    def push(self, value: Any) -> bool:
         """Add element to stack"""
         if len(self.elements) < self.max_length:
             self.elements.append(value)
             return True
         return False
 
-    def pop(self) -> Optional[Color]:
+    def pop(self) -> Optional[Any]:
         """Pop element from stack"""
         if self.elements:
             return self.elements.pop()
 
-    def peek(self) -> Optional[Color]:
+    def peek(self) -> Optional[Any]:
         """Peek top element"""
         if not self.empty:
             return self.elements[-1]
@@ -37,10 +37,10 @@ class Stack:
         """True if stack is empty"""
         return not len(self.elements)
 
-    def __iter__(self):
-        for el in list(self.elements):
-            yield el
-
+    @property
+    def full(self) -> bool:
+        """True if pile is full"""
+        return len(self.elements) == self.max_length
 
 class Pile(Stack):
     """Container for elements"""
@@ -62,7 +62,6 @@ class Pile(Stack):
         self.window = window
         self.block_width = width
         self.block_height = height
-        self.size = size
         self.x = x
         self.y = y
         self.padding = width // 2
@@ -70,11 +69,11 @@ class Pile(Stack):
             self.x - self.padding,
             self.y - self.padding,
             self.block_width + 2 * self.padding,
-            self.block_height * self.size + 2 * self.padding,
+            self.block_height * size + 2 * self.padding,
         )
         self.activated = False
         self.callback = callback
-        super().__init__(self.size)
+        super().__init__(size)
 
     def draw(self) -> None:
         """Draw elements"""
@@ -84,7 +83,7 @@ class Pile(Stack):
                 color.value,
                 (
                     self.x,
-                    self.y + (self.size - idx - 1) * self.block_height,
+                    self.y + (self.max_length - idx - 1) * self.block_height,
                     self.block_width,
                     self.block_height,
                 ),
@@ -98,21 +97,16 @@ class Pile(Stack):
                 self.x,
                 self.y,
                 self.block_width,
-                self.block_height * self.size,
+                self.block_height * self.max_length,
             ),
             self.BORDER_WIDTH,
         )
 
-    @property
-    def full(self) -> bool:
-        """True if pile is full"""
-        return len(self.elements) == self.size
-
-    def activate(self):
+    def activate(self) -> None:
         """Activate pile"""
         self.activated = True
 
-    def deactivate(self):
+    def deactivate(self) -> None:
         """Deactivate pile"""
         self.activated = False
 
@@ -135,7 +129,7 @@ class Pile(Stack):
         color = self.peek()
         if self.empty:
             return
-        for _ in range(pile.size - len(pile.elements)):
+        for _ in range(pile.max_length - len(pile.elements)):
             if self.peek() != color:
                 return
             pile.push(self.pop())
